fe01698e05e78e31221ed8b80cba8d60b51fe348
Justin Labak <jtlabak@gmail.com>
2022-11-30 22:40:12 -0800

    day1 part 2

--- src/day1/mod.rs
+++ src/day1/mod.rs
@@ -5,14 +5,24 @@ pub fn solve() {
     let contents = fs::read_to_string("./src/day1/input.txt").expect("Input failure");
     let cals = contents.split("\n").map(|a| a.trim());
 
-    let mut most = 0;
+    let mut most_1 = 0;
+    let mut most_2 = 0;
+    let mut most_3 = 0;
+
     let mut sum = 0;
 
     for cal in cals {
 
         if cal.len() == 0 {
-            if sum > most {
-                most = sum;
+            if sum > most_1 {
+                most_3 = most_2;
+                most_2 = most_1;
+                most_1 = sum;
+            } else if sum > most_2 {
+                most_3 = most_2;
+                most_2 = sum;
+            } else if sum > most_3 {
+                most_3 = sum;
             }
             sum = 0;
         } else {
@@ -21,5 +31,6 @@ pub fn solve() {
 
     }
 
-    println!("{most}");
+    let total = most_1 + most_2 + most_3;
+    println!("{total}");
 }
