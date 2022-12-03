af878f34b0443957309049a1191ca7c543e5ce1c
Justin Labak <jtlabak@gmail.com>
2022-12-02 16:25:49 -0800

    add a fast day2

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-mod d2;
+mod d2fast;
 
 fn main() {
 
     let input = include_str!("../input/02");
     let now = std::time::Instant::now();
-    let (p1, p2) = d2::solve(input);
+    let (p1, p2) = d2fast::solve(input);
 
     println!("Solution Part1:\t{}", p1);
     println!("Solution Part2:\t{}", p2);
