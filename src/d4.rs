a864d469262c054c17458fcf067718c01c7180eb
Justin Labak <jtlabak@gmail.com>
2022-12-03 22:56:42 -0800

    d4 with scan_fmt instead of regex

--- src/d4.rs
+++ src/d4.rs
@@ -1,17 +1,10 @@
-use regex::Regex;
+use scan_fmt::scan_fmt;
 
 pub fn solve(input: &str) -> (u16, u16) {
 
-    let re = Regex::new(r"(\d+)-(\d+),(\d+)-(\d+)").unwrap();
-
     return input.lines().fold((0, 0), |acc, line| {
 
-        // Parse integer range data from input.
-        let cap = re.captures(line).unwrap();
-        let x0: u16 = (&cap[1]).parse().unwrap();
-        let x1: u16 = (&cap[2]).parse().unwrap();
-        let y0: u16 = (&cap[3]).parse().unwrap();
-        let y1: u16 = (&cap[4]).parse().unwrap();
+        let (x0, x1, y0, y1) = scan_fmt!(line, "{d}-{d},{d}-{d}", u16, u16, u16, u16).unwrap();
 
         let contains = if (x0 >= y0 && x1 <= y1) || (y0 >= x0 && y1 <= x1) { 1 } else { 0 };
         let partial = if (x0 >= y0 && x0 <= y1) || (y0 >= x0 && y0 <= x1) { 1 } else { 0 };
