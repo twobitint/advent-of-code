db5c195fdd13697af504eef9da56975e70b2790a
Justin Labak <jtlabak@gmail.com>
2022-12-03 22:43:02 -0800

    day 4!

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,10 @@
-mod d3;
+mod d4;
 
 fn main() {
 
-    // let mut left: Vec<u8> = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".as_bytes().iter().map(|a| d3::atop(*a)).collect();
-    // println!("{:?}", left);
-
-    let input = include_str!("../input/03");
+    let input = include_str!("../input/04");
     let now = std::time::Instant::now();
-    let (p1, p2) = d3::solve(input);
+    let (p1, p2) = d4::solve(input);
 
     println!("Solution Part1:\t{}", p1);
     println!("Solution Part2:\t{}", p2);
