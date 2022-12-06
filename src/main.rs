08c82a3cba694d0434efd7b74559c9da5ace38ee
Justin Labak <jtlabak@gmail.com>
2022-12-05 22:54:51 -0800

    day 6 p1

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-mod d5;
+mod d6;
 
 fn main() {
 
-    let input = include_str!("../input/05");
+    let input = include_str!("../input/06");
     let now = std::time::Instant::now();
-    let (p1, p2) = d5::solve(input);
+    let (p1, p2) = d6::solve(input);
 
     println!("Solution Part1:\t{}", p1);
     println!("Solution Part2:\t{}", p2);
