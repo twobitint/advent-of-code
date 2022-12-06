2112c1b4afe214c391bd1e3d8bf818d4719156de
Justin Labak <jtlabak@gmail.com>
2022-12-06 10:58:01 -0800

    day 6

--- src/d6.rs
+++ src/d6.rs
@@ -1,15 +1,22 @@
+use std::collections::HashMap;
+
 pub fn solve(input: &str) -> (u16, u16) {
-    return (p1(input), 0);
+    return (marker(input, 4), marker(input, 14));
 }
 
-fn p1(input: &str) -> u16 {
+fn marker(input: &str, size: usize) -> u16 {
+    let mut map: HashMap<char, bool> = HashMap::new();
     let buf: Vec<char> = input.chars().collect();
-    for i in 0..buf.len()-4 {
-        if let [a, b, c, d] = &buf[i..i+4] {
-            if a != b && a != c && a != d && b != c && b != d && c != d {
-                return i as u16 + 4;
-            }
+    let mut iter = 0;
+    
+    while map.len() < size {
+        if !map.contains_key(&buf[iter]) {
+            map.insert(buf[iter], true);
+            iter += 1;
+        } else {
+            map.clear();
         }
     }
-    return 0;
+
+    return iter as u16;
 }
